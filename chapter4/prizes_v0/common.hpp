68d807ec40fc7a86e59d30c5e90e0ef9774ceb08
hadrianhu888 <hadrian.hu@gmail.com>
2023-02-21 07:56:14 -0500

    redid the prizes game again

--- chapter4/prizes_v0/common.hpp
+++ chapter4/prizes_v0/common.hpp
@@ -9,12 +9,6 @@
  *
  */
 
-#ifndef A7BB73E5_7AE5_4584_AE6E_AF03F68E0FD3
-#define A7BB73E5_7AE5_4584_AE6E_AF03F68E0FD3
-
-#ifndef B8C8C3EB_F813_4E0D_B50E_9CF4F78E6436
-#define B8C8C3EB_F813_4E0D_B50E_9CF4F78E6436
-
 #ifndef COMMON_HPP
 #define COMMON_HPP
 
@@ -36,13 +30,15 @@
 #include <cstdio> 
 #include <cstdlib>
 #include <bitset> 
+#include <vector>
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <time.h> 
 
 using namespace std;
 
 #endif
 
 #endif /* COMMON_HPP */
-#endif /* B8C8C3EB_F813_4E0D_B50E_9CF4F78E6436 */
-
-
-#endif /* A7BB73E5_7AE5_4584_AE6E_AF03F68E0FD3 */
