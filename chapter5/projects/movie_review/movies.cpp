44b50834ccdb1c07651336dfb8ee1171976c601a
hadrianhu888 <hadrian.hu@gmail.com>
2023-03-04 20:23:53 -0500

    updated movies.cpp

--- chapter5/projects/movie_review/movies.cpp
+++ chapter5/projects/movie_review/movies.cpp
@@ -44,6 +44,8 @@
 
 using namespace std;
 using namespace std::chrono;
+using namespace std::chrono_literals;
+using namespace std::this_thread;
 
 /**
  * @brief Program to read movie reviews from a file and display the average
@@ -65,12 +67,29 @@ void read_movies(ifstream &in_file, vector<string> &movies, vector<int> &reviews
 void read_user_reviews(vector<int> &user_reviews);
 void write_user_reviews(vector<int> &user_reviews);
 float calculate_average(vector<int> &user_reviews);
+void locate_nearest_neighbour(vector<string> &movies, vector<int> &reviews, float user_average);
 int main(int argc, char const **argv); 
 
 /**
  * @brief implement the functions above 
  * 
  */
+
+void locate_nearest_neighbour(vector<string> &movies, vector<int> &reviews, float user_average)
+{
+    float min_diff = 1000;
+    float min_index = 0;
+    for (int i = 0; i < reviews.size(); i++)
+    {
+        float diff = abs(user_average - reviews[i]);
+        if (diff < min_diff)
+        {
+            min_diff = diff;
+            min_index = i;
+        }
+    }
+    cout << "The movie closest to your average is: " << movies[min_index] << endl;
+}
 
 float average(vector<int> &reviews)
 {
@@ -147,14 +166,38 @@ int main(int argc, char const **argv)
 {
     vector<string> movies;
     vector<int> reviews;
-    read_movies(in_file, movies, reviews);
+    /**
+     * @brief read the file movies.txt
+     * use a new function to read the file
+     */
+    while (in_file)
+    {
+        if(in_file.is_open())
+        {
+            /**
+             * @brief display the file contents
+             * 
+             */
+            while(in_file)
+            {
+                string line;
+                getline(in_file, line);
+                cout << line << endl;
+            }
+        }
+        else
+        {
+            cout << "File not found" << endl;
+        }
+    }
     vector<int> user_reviews;
     read_user_reviews(user_reviews);
     write_user_reviews(user_reviews);
     float user_average = calculate_average(user_reviews);
     cout << "Your average review is: " << user_average << endl;
     float min_index = nearest_neighbor(movies, reviews, user_average);
-    cout << "The movie closest to your average is: " << movies[min_index] << endl;
+    cout << "The movie closest to your average is: " << endl; 
+    locate_nearest_neighbour(movies, reviews, user_average);
     return 0;
 }
 
